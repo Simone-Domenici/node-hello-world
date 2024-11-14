1a514cfab5e24262d0f0ad98de582bbc25205733
Simone-Domenici <domenici.simone00@gmail.com>
2024-11-14 16:35:57 +0100

    ADD version with sliced array of progress.argv

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const parameter = process.argv[2] ?? 'Boolean'
+const params = process.argv.slice(2)
 
-console.log(`Hello ${parameter}`)
+console.log(`Hello ${params[0] ?? 'Boolean'}`)
+
+
+// const parameter = process.argv[2] ?? 'Boolean'
+
+// console.log(`Hello ${parameter}`)
 
 // if (parameter === undefined) {
 //     console.log('Hello Boolean')   
